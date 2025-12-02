2afffd3c8b493eb791e2546d9013a6e893a504d0
Nikos Kostoulas <nikos@engagework.com>
2025-12-02 11:52:19 +0200

    2025 d2

--- src/y2025/d2.rs
+++ src/y2025/d2.rs
@@ -1,112 +1,109 @@
-enum Direction {
-    Right(i32),
-    Left(i32),
-}
-
-impl Direction {
-    fn new(s: &str) -> Self {
-        let mut chars = s.chars();
-        let dir = chars.next().unwrap();
-        let number: i32 = chars.collect::<String>().parse::<i32>().unwrap();
-        if dir == 'R' {
-            Direction::Right(number)
-        } else {
-            Direction::Left(number)
-        }
-    }
-
-    fn add(&self, num: i32) -> i32 {
-        match self {
-            Direction::Right(val) => (val + num) % 100,
-            Direction::Left(val) => (num - val + 100) % 100,
-        }
-    }
-
-    fn add_2(&self, acc: (i32, i32)) -> (i32, i32) {
-        let mut pwd = acc.1;
-        let num = acc.0;
-        let value = match self {
-            Direction::Right(val) => {
-                pwd += (val + num) / 100;
-                (val + num) % 100
-            }
-            Direction::Left(val) => {
-                pwd += (num - val).abs() / 100;
-                if num != 0 && *val >= num {
-                    pwd += 1;
-                }
-                (num - (val % 100) + 100) % 100
-            }
-        };
-
-        (value, pwd)
-    }
-}
+use parser::Parser;
 
 pub fn part1(input: String) -> String {
-    let lists = parse(&input);
-    let password = lists.iter().fold((50, 0), |acc, v| {
-        let mut pwd = acc.1;
-        let value = v.add(acc.0);
-
-        if value == 0 {
-            pwd += 1
-        }
-        (value, pwd)
-    });
-
-    password.1.to_string()
+    let ranges = parse(&input);
+    ranges
+        .iter()
+        .flat_map(|r| range_to_invalid_nums(r, is_palindromic))
+        .reduce(|acc, b| acc + b)
+        .unwrap()
+        .to_string()
 }
 
 pub fn part2(input: String) -> String {
-    let lists = parse(&input);
-    let password = lists.iter().fold((50, 0), |acc, v| v.add_2(acc));
-
-    password.1.to_string()
+    let ranges = parse(&input);
+    ranges
+        .iter()
+        .flat_map(|r| range_to_invalid_nums(r, is_multi_palindromic))
+        .reduce(|acc, b| acc + b)
+        .unwrap()
+        .to_string()
 }
 
-fn parse(input: &str) -> Vec<Direction> {
-    let lines: Vec<String> = input.split('\n').map(|s| s.to_string()).collect();
-    let lists: Vec<Direction> = lines
+pub fn range_to_invalid_nums<F>(range: &(u64, u64), is_invalid_cb: F) -> Vec<u64>
+where
+    F: Fn(u64) -> bool,
+{
+    let mut result = vec![];
+    for i in range.0..=range.1 {
+        if is_invalid_cb(i) {
+            result.push(i);
+        }
+    }
+    result
+}
+
+pub fn is_palindromic(num: u64) -> bool {
+    let str = num.to_string();
+    let length = str.len();
+    if length.is_multiple_of(2) && str[0..length / 2] == str[length / 2..] {
+        return true;
+    }
+
+    false
+}
+
+pub fn is_multi_palindromic(num: u64) -> bool {
+    let str = num.to_string();
+    let length = str.len();
+    for i in 1..length {
+        if length.is_multiple_of(i) {
+            let com = &str[0..i];
+            let mut same = true;
+
+            for j in 1..(length / i) {
+                if str[(i * j)..((j + 1) * i)] != *com {
+                    same = false;
+                }
+            }
+            if same {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+fn parse(input: &str) -> Vec<(u64, u64)> {
+    let parser: Parser = Parser::new(input);
+    let ranges: Vec<String> = parser.split_to_strings(",");
+    let ranges_num = ranges
         .iter()
-        .filter(|s| !s.is_empty())
-        .map(|s| Direction::new(s))
+        .map(|s| {
+            let nums: Vec<String> = s.split("-").map(|s| s.to_string()).collect();
+            let from = nums.get(0).unwrap().parse::<u64>().unwrap();
+            let to = nums.get(1).unwrap().parse::<u64>().unwrap();
+            (from, to)
+        })
         .collect();
-    lists
+
+    ranges_num
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const INPUT: &str = "L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82";
+    const INPUT: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
     #[test]
     fn test_example_part1() {
         let result = part1(INPUT.to_string());
-        assert_eq!(result, "3");
+        assert_eq!(result, "1227775554");
     }
 
     #[test]
     fn test_example_part2() {
         let result = part2(INPUT.to_string());
-        assert_eq!(result, "6");
+        assert_eq!(result, "4174379265");
     }
 
     #[test]
     #[cfg(feature = "test_input")]
     fn test_part1_input() {
         assert_eq!(
-            part1(include_str!("../../.data/y2025/d1.txt").to_string()),
-            "1034"
+            part1(include_str!("../../.data/y2025/d2.txt").to_string()),
+            "31210613313"
         );
     }
 
@@ -114,8 +111,8 @@ L82";
     #[cfg(feature = "test_input")]
     fn test_part2_input() {
         assert_eq!(
-            part2(include_str!("../../.data/y2025/d1.txt").to_string()),
-            "6166"
+            part2(include_str!("../../.data/y2025/d2.txt").to_string()),
+            "41823587546"
         );
     }
 }
